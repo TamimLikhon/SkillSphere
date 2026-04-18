58a8f63cbac2124cdeaeade0f55853fa73a2b85f
ShirinAkter56 <23201056@uap-bd.edu>
2026-04-18 21:42:42 +0600

    Added interviews app with models, views, admin and templates

---
+++ skillsphere/recruitment/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import RecruiterProfile, CandidateProfile
+
+# Register your models here.
+admin.site.register(RecruiterProfile)
+admin.site.register(CandidateProfile)
