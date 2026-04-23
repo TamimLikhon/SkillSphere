305fc6c5f52016f097f1143b85d4c97b7a2c0027
annotomamirdha123 <annotomamirdha123@gmail.com>
2026-04-23 23:35:55 +0600

    add views and urls configuration

--- skillsphere/jobs/views.py
+++ skillsphere/jobs/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import JobPost, Application
+from .forms import JobPostForm, ApplicationForm
+from django.contrib.auth.decorators import login_required
+
+
+def job_list(request):
+    jobs = JobPost.objects.filter(status='open')
+    return render(request, 'jobs/job_list.html', {'jobs': jobs})
+
+
+def job_detail(request, pk):
+    job = get_object_or_404(JobPost, pk=pk)
+    return render(request, 'jobs/job_detail.html', {'job': job})
+
+
+@login_required
+def job_create(request):
+    if request.user.role != 'recruiter':
+        return redirect('job_list')
+
+    if request.method == 'POST':
+        form = JobPostForm(request.POST)
+        if form.is_valid():
+            job = form.save(commit=False)
+            job.recruiter = request.user.recruiterprofile
+            job.save()
+            return redirect('job_list')
+    else:
+        form = JobPostForm()
+
+    return render(request, 'jobs/job_form.html', {'form': form})
+
+
+@login_required
+def apply_job(request, pk):
+    job = get_object_or_404(JobPost, pk=pk)
+
+    if request.user.role != 'candidate':
+        return redirect('job_list')
+
+    if request.method == 'POST':
+        form = ApplicationForm(request.POST)
+        if form.is_valid():
+            app = form.save(commit=False)
+            app.candidate = request.user.candidateprofile
+            app.job = job
+            app.save()
+            return redirect('job_list')
+    else:
+        form = ApplicationForm()
+
+    return render(request, 'jobs/apply.html', {'form': form, 'job': job})
